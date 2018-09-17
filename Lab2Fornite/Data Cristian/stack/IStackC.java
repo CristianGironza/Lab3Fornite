8fb906e4603ce2bfff089cc7b8022eea172ec9aa
njse22 <njse22@gmail.com>
2018-09-16 19:06:01 -0500

    the comments of stack

--- Lab2Fornite/Data Cristian/stack/IStackC.java
+++ Lab2Fornite/Data Cristian/stack/IStackC.java
@@ -3,27 +3,28 @@ package stack;
 public interface IStackC <T> {
 
 	/**
-	 * 
-	 * @return
+	 * isEmpty() : query if the list be empty or not 
+	 * @return   : true  - if the stack is empty
+	 * 			 : false - if the stack is not empty
 	 */
 	public boolean empty();
 	
 	/**
-	 * 
-	 * @return
+	 * peek()    : query the element in the stack without take this element 
+	 * @return T : the element that has been query
 	 */
 	public T peek();
 	
 	/**
-	 * 
-	 * @return
+	 * pop()     : remove the object at the top of this stack 
+	 * @return T : that object as the value of this function.
 	 */
 	public T pop() throws StackCException;
 	
 	/**
-	 * 
-	 * @param item
-	 * @return
+	 * push()        : add element to the final of this stack  
+	 * @param item T : the object that has been added to the stack  
+	 * @return       : the item argument
 	 */
 	public void push(T item);
 	
--- Lab2Fornite/Data Cristian/stack/StackC.java
+++ Lab2Fornite/Data Cristian/stack/StackC.java
@@ -7,6 +7,11 @@ public class StackC <T> implements IStackC<T> {
 	
 	private ISimpleNodeC<T> first;
 	
+	/**
+	 * isEmpty() : query if the list be empty or not 
+	 * @return   : true  - if the stack is empty
+	 * 			 : false - if the stack is not empty
+	 */
 	@Override
 	public boolean empty() {
 		boolean empty = true;
@@ -16,6 +21,10 @@ public class StackC <T> implements IStackC<T> {
 		return empty;
 	}
 
+	/**
+	 * peek()    : query the element in the stack without take this element 
+	 * @return T : the element that has been query
+	 */
 	@Override
 	public T peek() {
 		T item = null;
@@ -25,6 +34,10 @@ public class StackC <T> implements IStackC<T> {
 		return item;
 	}
 
+	/**
+	 * pop()     : remove the object at the top of this stack 
+	 * @return T : that object as the value of this function.
+	 */
 	@Override
 	public T pop() throws StackCException {
 		if(!empty()) {
@@ -36,6 +49,11 @@ public class StackC <T> implements IStackC<T> {
 		}
 	}
 
+	/**
+	 * push()        : add element to the final of this stack  
+	 * @param item T : the object that has been added to the stack  
+	 * @return       : the item argument
+	 */
 	@Override
 	public void push(T item) {
 		ISimpleNodeC<T> node = new SimpleNodeC<T>(item);
